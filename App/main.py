e15c825a033f9a2a3b17d8f8b0ff63afa6c526ff
edwmn01 <mendieta.c.gamer@gmail.com>
2025-07-05 19:25:42 -0500

    Proyecto v0.0.3

--- App/main.py
+++
@@ -1,9 +0,0 @@
-from modulos.vistas import componentes as cmp
-
-def main():
-    cmp.configurar_pagina()
-    pagina = cmp.menu_principal()
-    cmp.actualizar_pagina(pagina)
-
-
-main()
--- README.md
+++ README.md
@@ -1,9 +1,1 @@
-# Proyecto-Final-Ciclo-3
-
-## Dependencias:
-- streamlit
-- mysql-connector-python
-- python-dotenv
-- pandas
-- openpyxl
-- xlrd
+## Proyecto final ciclo 3
