6f27a5babb41864a1cc363e79e73ad6916b9daa1
rezeranty <antomrosillo513@gmail.com>
2025-07-10 23:28:56 -0500

    agregando limpieza de datos para base de fichas.

--- Modulos/etl_limpieza.py
+++ Modulos/etl_limpieza.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import numpy as np
 import os
+from datetime import datetime
 
-
-base_path = "data"
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+base_path = os.path.join(BASE_DIR, "data")
 
 def limpiar_notas():
     df_notas_20231p = pd.read_excel(os.path.join(base_path, "NOTAS_2023_1P.xlsx"), dtype={"ci_pasaporte": str})
@@ -36,5 +37,67 @@ def limpiar_notas():
 
 
 def limpiar_fichas():
-    pass
+    df_fichas_20231p = pd.read_excel(os.path.join(BASE_DIR, "data", "Fichas_2023_1P.xlsx"), dtype={"ci_pasaporte": str})
+    df_fichas_20232p = pd.read_excel(os.path.join(BASE_DIR, "data", "Fichas_2023_2P.xlsx"), dtype={"ci_pasaporte": str})
+    df_fichas_20241p = pd.read_excel(os.path.join(BASE_DIR, "data", "Fichas_2024_1P.xlsx"), dtype={"ci_pasaporte": str})
+    df_fichas_20242p = pd.read_excel(os.path.join(BASE_DIR, "data", "Fichas_2024_2P.xlsx"), dtype={"ci_pasaporte": str})
+    df_fichas_20251p = pd.read_excel(os.path.join(BASE_DIR, "data", "Fichas_2025_1P.xlsx"), dtype={"ci_pasaporte": str})
 
+    df_fichas = pd.concat([df_fichas_20231p, df_fichas_20232p, df_fichas_20241p, df_fichas_20242p, df_fichas_20251p],
+                        ignore_index=True)
+    df_fichas["ci_pasaporte"] = df_fichas["ci_pasaporte"].str.zfill(10)
+    df_fichas = df_fichas.astype(str)
+    
+    for col in df_fichas.columns:
+        if col != "correo_tec":
+            df_fichas[col] = df_fichas[col].str.strip().str.upper()
+            df_fichas[col] = df_fichas[col].replace(".", "", regex=False)
+            df_fichas[col] = df_fichas[col].fillna("DESCONOCIDO")
+
+ 
+    df_fichas["nombres"] = (df_fichas["PrimerNombre"] + " " +
+        df_fichas["SegunNombre"] + " " +
+        df_fichas["PrimerApellido"] + " " +
+        df_fichas["SegunApellido"]
+    )
+                
+    df_fichas = df_fichas.drop(columns=["PrimerNombre", "SegunNombre", "PrimerApellido", "SegunApellido"])
+    
+    columnas_enteras = ["num_hijos", "anio_graduacion", "num_propiedades", "num_vehiculos", "semanas_embarazo"]
+    columnnas_flotantes =["valor_propiedades", "valor_vehiculos", "total_ingresos", "total_egresos", "porcentaje_discapacidad"]
+    columnas_booleanas = ["tiene_beca","estudio_otra_carrera","recibe_ayuda"]
+
+    for col in columnas_enteras:
+        df_fichas[col] = df_fichas[col].str.replace(r"[^0-9]", "", regex=True).replace("",np.nan).fillna(0).astype(int)
+
+    for col in columnnas_flotantes:
+        df_fichas[col] = df_fichas[col].str.replace(r"[^0-9]", "", regex=True).replace("",np.nan).fillna(0).astype(float)
+
+    for col in columnas_booleanas:
+        df_fichas[col] = df_fichas[col] == "SÍ"
+
+    df_fichas["fecha_nacimiento"] = pd.to_datetime(df_fichas["fecha_nacimiento"], errors='coerce', dayfirst=True)
+    df_fichas["fecha_nacimiento"] = df_fichas["fecha_nacimiento"].fillna(pd.Timestamp.now())
+    df_fichas["fecha_nacimiento"] = df_fichas["fecha_nacimiento"].dt.strftime("%d/%m/%Y")
+
+
+    columnas_str = df_fichas.select_dtypes(include=["string", "object"]).columns
+    df_fichas[columnas_str] = df_fichas[columnas_str].fillna("DESCONOCIDO")
+    
+    columnas_num = df_fichas.select_dtypes(include=["number"]).columns
+    df_fichas[columnas_num] = df_fichas[columnas_num].fillna(0)
+    
+    print(df_fichas.isnull().sum().sort_values(ascending=False))
+
+    df_fichas[columnas_str] = df_fichas[columnas_str].replace(
+        to_replace=r"^\s*(nan|NaN|NAN)\s*$",
+        value="DESCONOCIDO",
+        regex=True
+    )
+
+    df_fichas.to_excel(os.path.join(BASE_DIR, "data", "fichas_limpias_final.xlsx"), index=False)
+
+
+
+limpiar_fichas()
+limpiar_notas()    
